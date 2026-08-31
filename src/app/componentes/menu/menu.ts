6da4a2c814a924e1c69dabe0166c0787dd9e8caf
gustavo-mota8 <gustavvomota8@gmail.com>
2026-08-31 16:43:40 -0300

    feat: criando menu hamburguer

--- src/app/componentes/menu/menu.ts
+++ src/app/componentes/menu/menu.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu {
+
+  elemento = false;
+
+  mostrarElemento() {
+
+    this.elemento = !this.elemento;
+
+  var aside = document.querySelector(".menu-lateral");
+
+
+    
+  } 
 
 }
